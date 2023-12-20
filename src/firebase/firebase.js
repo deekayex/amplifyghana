dff4e214493b2d0e08d444cbb271459ea12fcf6f
unknown <deekayexq@gmail.com>
2023-12-20 21:54:53 +0000

    Setting up backend

---
+++ src/firebase/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCdOY0enT1p3Ao12Ed2Lkpij1gLjE0wpX0",
+  authDomain: "amplify-gh.firebaseapp.com",
+  projectId: "amplify-gh",
+  storageBucket: "amplify-gh.appspot.com",
+  messagingSenderId: "553417073189",
+  appId: "1:553417073189:web:817035b8e969ab721dd5cc",
+  measurementId: "G-LNG46DCGZD"
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const database = getFirestore(app);
