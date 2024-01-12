517484fb5be71c5b67acf8e34eb97c09fae59f13
unknown <deekayexq@gmail.com>
2024-01-12 08:59:15 +0000

    s

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,20 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from 'firebase/storage';
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCdOY0enT1p3Ao12Ed2Lkpij1gLjE0wpX0",
-  authDomain: "amplify-gh.firebaseapp.com",
-  projectId: "amplify-gh",
-  storageBucket: "amplify-gh.appspot.com",
-  messagingSenderId: "553417073189",
-  appId: "1:553417073189:web:817035b8e969ab721dd5cc",
-  measurementId: "G-LNG46DCGZD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 
