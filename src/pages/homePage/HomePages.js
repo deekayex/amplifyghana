f9cfb94325e7e2adc64b475b35eb7d5241581c93
unknown <deekayexq@gmail.com>
2024-01-28 19:55:00 +0000

    Code optimising

--- src/pages/homePage/HomePages.js
+++ src/pages/homePage/HomePages.js
@@ -1,34 +1,41 @@
-import React from 'react'
-import './HomePages.css'
-import Home from '../../components/Home'
+import React, { lazy, Suspense } from 'react';
+import './HomePages.css';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import News from '../news/News';
-import EditorsPicks from '../editorsPicks/EditorsPicks';
 import Socials from '../../components/socials/Socials';
+import Home from '../../components/Home';
 
 
+const News = lazy(() => import('../news/News'));
+const EditorsPicks = lazy(() => import('../editorsPicks/EditorsPicks'));
 
 function HomePages() {
+  
   return (
-    <div className='container'id='home'>
+    <div className='container' id='home'>
       <div>
-         <div className='home-page'>
+        <div className='home-page'>
           <div className='space'/>
           <div className='home-menu-icons'>
             <div className='home-icon'>
-               <HomeOutlinedIcon fontSize='10rem' className='home_icon'/>
-               HOME
-               </div>
-               <div className='home-socials'><Socials/></div>
+              <HomeOutlinedIcon fontSize='10rem' className='home_icon'/>
+              HOME
+            </div>
+            <div className='home-socials'><Socials/></div>
           </div>
-            <Home/>
-            <News/>
-            <EditorsPicks/>
-          </div>
+
+          <Home />
+
+          <Suspense fallback={<div>Loading...</div>}>
+            {/* Lazy-loaded News component */}
+            <News />
+            {/* Lazy-loaded EditorsPicks component */}
+            <EditorsPicks />
+          </Suspense>
+
+        </div>
       </div>
     </div>
-        
-  )
+  );
 }
 
-export default HomePages
+export default HomePages;
