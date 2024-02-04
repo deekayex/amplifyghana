9ca1beb35d7bf50274e1a590a643e616b3c907b9
unknown <deekayexq@gmail.com>
2024-02-04 12:45:05 +0000

    Change to speed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import App from './App'
 import './index.css'
 import  ReactDOM  from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
