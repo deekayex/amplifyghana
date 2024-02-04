182678f72f0740b8749988e5a2c8a1e0d553fb3e
unknown <deekayexq@gmail.com>
2024-02-04 12:38:09 +0000

    Speed insights

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import App from './App'
 import './index.css'
 import  ReactDOM  from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     {/* <Analytics> */}
       <App />
     <Analytics/>
+    <SpeedInsights />
   </React.StrictMode>
 );
