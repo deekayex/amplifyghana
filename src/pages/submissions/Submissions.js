be7b4514716cff103d60e05833fefaaea75e422c
unknown <deekayexq@gmail.com>
2024-02-07 21:46:35 +0000

    Changed database location

--- src/pages/submissions/Submissions.js
+++ src/pages/submissions/Submissions.js
@@ -14,8 +14,8 @@ function Submissions() {
     <section className='submissions-container' id='submissions'>
        <Helmet>
         <title>Submissions | Amplify Ghana</title>
-        <meta name="description" content= "Amplify Ghana is an Online Creative’s Promotion and Public Relations Agency Founded in 2020. Our Primary Mission is to Elevate Creatives Throughout Africa, With a Special Focus on Ghana, As that is Where We are Headquartered." />
         <meta property="og:image" content='https://ucarecdn.com/04cd01ca-f483-421d-acad-b64ab26cd7f1/sharelogo.png'/>
+        <meta name="description" content= "Amplify Ghana is an Online Creative’s Promotion and Public Relations Agency Founded in 2020. Our Primary Mission is to Elevate Creatives Throughout Africa, With a Special Focus on Ghana, As that is Where We are Headquartered." />
       </Helmet>
       <ScrollToTopOnMount/>
       <div className='space' />
