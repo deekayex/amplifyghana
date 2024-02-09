1d37c50315017e12eaa596a7cb753917ec85ba5f
unknown <deekayexq@gmail.com>
2024-02-09 07:18:27 +0000

    Modifying article helmet

--- src/context/cache/cacheUtils.js
+++ src/context/cache/cacheUtils.js
@@ -1,4 +1,3 @@
-// cacheUtils.js
 import cache from 'memory-cache';
 
 export const fetchDataWithCache = async (cacheKey, fetchDataFromServer) => {
@@ -7,8 +6,13 @@ export const fetchDataWithCache = async (cacheKey, fetchDataFromServer) => {
   if (cachedData) {
     return cachedData;
   } else {
-    const newData = await fetchDataFromServer();
-    cache.put(cacheKey, newData, 3600 * 1000); // Cache for 1 hour
-    return newData;
+    try {
+      const newData = await fetchDataFromServer();
+      cache.put(cacheKey, newData, 3600 * 1000); // Cache for 1 hour
+      return newData;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error; // Re-throw the error to handle it at a higher level if needed
+    }
   }
 };
