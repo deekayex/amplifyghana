94105c9b80eb272aa5fbc1035d22695e5b62d5be
unknown <deekayexq@gmail.com>
2024-02-14 11:32:57 +0000

    prerendering

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import App from './App'
-import './index.css'
-import  ReactDOM  from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
+const rootElement = document.getElementById('root');
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
+    <React.StrictMode>
       <App />
-    <Analytics/>
-    <SpeedInsights />
-  </React.StrictMode>
-);
+      <Analytics />
+      <SpeedInsights />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+      <Analytics />
+      <SpeedInsights />
+    </React.StrictMode>,
+    rootElement
+  );
+}
